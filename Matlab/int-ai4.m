255c3c18818a33cb3611ecc5e836b151cb39444d
NoujoudNader <101670658+NoujoudNader@users.noreply.github.com>
2024-05-07 21:16:51 -0500

    Update integration version 4

--- Matlab/int-ai4.m
+++ Matlab/int-ai4.m
@@ -1,1 +1,12 @@
+% Define the function to be integrated
+f = @(x) abs(sin(x));
 
+% Set the limits of integration
+a = -pi;
+b = 2*pi/3;
+
+% Perform the integration using the integral function
+area = integral(f, a, b);
+
+% Display the result
+fprintf('The total area between sin(x) and the x-axis from -pi to 2/3*pi is: %.4f\n', area);
